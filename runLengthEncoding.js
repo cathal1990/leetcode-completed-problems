aadd7fa2bceb9498672ac6184eca81b6cd6863be
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-11-22 00:26:50 +0000

    Completed

---
+++ runLengthEncoding.js
@@ -0,0 +1,56 @@
+// eg 'AAA' => '3A'
+// eg 'AABBCCBF' => '2A2B2C1B1F'
+
+// If over 9 then split to single
+// eg 'AAAAAAAAAAAA' => '9A3A'
+
+
+function runLengthEncoding(string) {
+    // Write your code here.
+    let count = 1;
+    let result = '';
+
+    for (let i=0; i < string.length; i++) {
+      if (i === string.length - 1) {
+        if (string[i] === string[i-1]) {
+          if (count < 10) {
+          result += `${count}${string[i]}`
+          count = 1
+        }
+        else {
+          while (count >= 10) {
+            result += `9${string[i]}`
+            count -= 9
+          }
+          result += `${count}${string[i]}`
+          count = 1;
+        }
+          break;
+        }
+        else {
+          result += `1${string[i]}`
+          break;
+        }
+      }
+      if (string[i] === string[i+1]) {
+        count++;
+        continue;
+      }
+      else {
+        if (count < 10) {
+          result += `${count}${string[i]}`
+          count = 1
+        }
+        else {
+          while (count >= 10) {
+            result += `9${string[i]}`
+            count -= 9
+          }
+          result += `${count}${string[i]}`
+          count = 1;
+        }
+      }
+    }
+
+    return result
+  }
