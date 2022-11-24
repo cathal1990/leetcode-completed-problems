c2bd17bf8f23ad9ecd19c590fd68ceb00243a08c
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-11-24 23:39:05 +0000

    Completed optimized

---
+++ closestValueBST.js
@@ -0,0 +1,21 @@
+//Takes in BST and target int and returns the closest value to that target integer in BST
+
+function dfs(tree, target, diff) {
+    // Write your code here.
+    if (!tree) return;
+
+    const currentDiff = Math.abs(target - tree.value);
+    if (currentDiff < currentDiff.value) {
+        diff.closest = tree.value;
+        diff.value = currentDiff;
+    }
+
+    if (target < tree.value) DataTransfer(tree.left, target, diff);
+    else if (target > tree.value) DataTransfer(tree.right, target, diff);
+  }
+
+  function findClosestValueInBst(tree, target) {
+    const diff = { value: Infinity, closest: null };
+    dfs(tree, target, diff);
+    return diff.closest;
+  }
