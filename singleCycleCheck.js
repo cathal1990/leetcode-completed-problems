a3004d690fa47bb63f4e6cc0f5da78ef215d8fc2
Cathal O Cuinneagain <cathal1990@gmail.com>
2023-02-23 00:39:36 +0000

    youngestCommonAncestor completed

--- singleCycleCheck.js
+++ singleCycleCheck.js
@@ -3,4 +3,4 @@ function hasSingleCycle(array) {
     let jumpTo = array.map((val, i) => ((i + val) % array.length) % array.length);
     let noPairs = jumpTo.every((position, i) => jumpTo[position] !== i);
     return (new Set(jumpTo)).size === jumpTo.length && noPairs
-  }
+}
