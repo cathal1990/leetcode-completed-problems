6ac1e2ab7afc5e83f6259e4bf6b3fd567471c491
Cathal O Cuinneagain <cathal1990@gmail.com>
2023-03-18 12:04:37 +0000

    quickSort completed

---
+++ quickSort.js
@@ -0,0 +1,18 @@
+function quickSort(array) {
+    if (!array.length) {
+      return array
+    }
+    else {
+      let pivot = array[0]
+      let less = []
+      let equal = []
+      let more = []
+  
+      for (let i of array) {
+        if (i < pivot) less.push(i)
+        if (i == pivot) equal.push(i)
+        if (i > pivot) more.push(i)
+      }
+    return [...quickSort(less), ...equal, ...quickSort(more)]
+    }
+  }
